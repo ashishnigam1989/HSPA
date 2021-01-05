26b0e378eb82c42b072f884bb98020e3c2551045
Ashish <ashishnigam1989@gmail.com>
2021-01-05 16:55:15 +0530

    Project and GIT setup

---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-property-card',
+  //template: `<h1>I am card</h1>`,
+  templateUrl: 'property-card.component.html',
+  styleUrls: ['property-card.component.css']
+  //styles: ['h1 {font-weight:normal;}']
+})
+
+export class PropertyCardComponent {
+  Property: any = {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 15000
+  }
+}
